69a109dff34a77b84d793a808a92f09953b34053
DiegoSA <diego.sa.pb@gmail.com>
2017-04-24 12:29:56 -0300

    criei novas tabelas: vendedor motorista veiculo e adicioneis as telas adm, vendedor, motorista e cliente

---
+++ app/src/main/java/com/example/diego/rotas/telas/TelaPrincipalVendedor.java
@@ -0,0 +1,15 @@
+package com.example.diego.rotas.telas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.diego.rotas.R;
+
+public class TelaPrincipalVendedor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal_vendedor);
+    }
+}
