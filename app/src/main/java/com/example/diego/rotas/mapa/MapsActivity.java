c2b0ea457cf3bf1c4b6a1a79ab91c21e076db08a
DiegoSA <diego.sa.pb@gmail.com>
2017-05-15 23:42:24 -0300

    coloquei o código que gera os marcadores no mapa, falta transformar em método

--- app/src/main/java/com/example/diego/rotas/mapa/MapsActivity.java
+++ app/src/main/java/com/example/diego/rotas/mapa/MapsActivity.java
@@ -1,19 +1,41 @@
 package com.example.diego.rotas.mapa;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.location.Geocoder;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
 
 import com.example.diego.rotas.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+import static com.google.android.gms.maps.model.BitmapDescriptorFactory.HUE_GREEN;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private int opcao;
+    private ArrayList<Marker> entregas; //ainda estou em dúvida de como utilizar o armazenamento de marcadores
+    private LocationManager location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +60,95 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        location = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        location.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) MapsActivity.this);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // defino a latitude e longitude do marcador e adiciono no mapa
+        LatLng entrega1 = new LatLng(-7.12, -34.879);
+        mMap.addMarker(new MarkerOptions().position(entrega1).title("Primeira Entrega"));
+        LatLng entrega2 = new LatLng(-7.15, -34.8);
+        mMap.addMarker(new MarkerOptions().position(entrega2).title("Segunda Entrega"));
+        LatLng entrega3 = new LatLng(-7.13, -34.85);
+        mMap.addMarker(new MarkerOptions().position(entrega3).title("Terceira Entrega"));
+
+        //foco no primeiro marcador
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(entrega1));
+
+        //método para capturar o touch no marcador
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(final Marker marker) {
+
+                AlertDialog.Builder ad = new AlertDialog.Builder(MapsActivity.this);
+                ad.setTitle("Confirmação de Entrega");
+                ad.setMessage("A Entrega foi Finalizada?");
+                ad.setPositiveButton("SIM", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MapsActivity.this, "Entrega realizada", Toast.LENGTH_SHORT).show();
+                        opcao = 1;
+                        alterPosition(marker, opcao);
+                    }
+                });
+
+                ad.setNegativeButton("NÃO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MapsActivity.this, "Entrega não realizada", Toast.LENGTH_SHORT).show();
+                        AlertDialog.Builder adNao = new AlertDialog.Builder(MapsActivity.this);
+                        adNao.setTitle("Motivo da Não Entrega");
+                        adNao.setMessage("Devolução ou Reentrega?");
+                        adNao.setPositiveButton("Devolução", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                opcao = 2;
+                                alterPosition(marker, opcao);
+                            }
+                        });
+                        adNao.setNegativeButton("Reentrega", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                opcao = 3;
+                                alterPosition(marker, opcao);
+                            }
+                        });
+                        adNao.show();
+                    }
+                });
+
+                ad.show();
+
+                return false;
+            }
+        });
+
+        //zoom no marcador focado
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
+    }
+
+    public void alterPosition(Marker marker, int opcao){
+        switch (opcao){
+            case 1:
+                marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                break;
+            case 2:
+                marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+                break;
+            case 3:
+                marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                break;
+            default:
+                break;
+        }
     }
 }
